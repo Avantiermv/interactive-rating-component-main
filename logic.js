7adcba393c3c81262e007b2ecea2922e96d0e0e9
marcos vinicius <avantiermv@gmail.com>
2024-01-27 14:01:10 -0300

    .

--- logic.js
+++ logic.js
@@ -15,35 +15,174 @@ function pressionar1(){
      num1Value = 1;
      num1.style.color = 'hsl(0, 0%, 100%)'
      num1.style.backgroundColor = 'hsl(25, 97%, 53%)';
+
+     //pressionar2
+     if (num2Value == 1){
+        num2Value = 0;
+        num2.style.color = '';
+        num2.style.backgroundColor = '';
+     }
+
+     //Pressionar3
+    if (num3Value == 1){
+        num3Value = 0;
+        num3.style.color = '';
+        num3.style.backgroundColor = '';
+    }
+
+    //pressionar4
+    if (num4Value == 1){
+        num4Value = 0;
+        num4.style.color = '';
+        num4.style.backgroundColor = '';
+    }
+
+    //pressionar5
+    if (num5Value == 1){
+        num5Value = 0;
+        num5.style.color = '';
+        num5.style.backgroundColor = '';
+    }
 }
 
 function pressionar2(){
     num2Value = 1;
-    num2.style.color = 'hsl(0, 0%, 100%)'
+    num2.style.color = 'hsl(0, 0%, 100%)';
     num2.style.backgroundColor = 'hsl(25, 97%, 53%)';
+
+    //pressionar1
+    if (num1Value == 1){
+        num1Value = 0;
+        num1.style.color = '';
+        num1.style.backgroundColor = '';
+    }
+
+     //Pressionar3
+     if (num3Value == 1){
+        num3Value = 0;
+        num3.style.color = '';
+        num3.style.backgroundColor = '';
+    }
+
+    //pressionar4
+    if (num4Value == 1){
+        num4Value = 0;
+        num4.style.color = '';
+        num4.style.backgroundColor = '';
+    }
+
+    //pressionar5
+    if (num5Value == 1){
+        num5Value = 0;
+        num5.style.color = '';
+        num5.style.backgroundColor = '';
+    }
+
 }
 
 function pressionar3(){
     num3Value = 1;
     num3.style.color = 'hsl(0, 0%, 100%)'
     num3.style.backgroundColor = 'hsl(25, 97%, 53%)';
+
+    //pressionar1
+    if (num1Value == 1){
+        num1Value = 0;
+        num1.style.color = '';
+        num1.style.backgroundColor = '';
+    }
+
+     //Pressionar2
+     if (num2Value == 1){
+        num2Value = 0;
+        num2.style.color = '';
+        num2.style.backgroundColor = '';
+    }
+
+    //pressionar4
+    if (num4Value == 1){
+        num4Value = 0;
+        num4.style.color = '';
+        num4.style.backgroundColor = '';
+    }
+
+    //pressionar5
+    if (num5Value == 1){
+        num5Value = 0;
+        num5.style.color = '';
+        num5.style.backgroundColor = '';
+    }
 }
 
 function pressionar4(){
     num4Value = 1;
     num4.style.color = 'hsl(0, 0%, 100%)'
     num4.style.backgroundColor = 'hsl(25, 97%, 53%)';
+
+    //pressionar1
+    if (num1Value == 1){
+        num1Value = 0;
+        num1.style.color = '';
+        num1.style.backgroundColor = '';
+    }
+
+     //Pressionar3
+     if (num3Value == 1){
+        num3Value = 0;
+        num3.style.color = '';
+        num3.style.backgroundColor = '';
+    }
+
+    //pressionar2
+    if (num2Value == 1){
+        num2Value = 0;
+        num2.style.color = '';
+        num2.style.backgroundColor = '';
+    }
+
+    //pressionar5
+    if (num5Value == 1){
+        num5Value = 0;
+        num5.style.color = '';
+        num5.style.backgroundColor = '';
+    }
 }
 
 function pressionar5(){
     num5Value = 1;
     num5.style.color = 'hsl(0, 0%, 100%)'
     num5.style.backgroundColor = 'hsl(25, 97%, 53%)';
-}
-function pressionar(){
+
+    //pressionar1
     if (num1Value == 1){
-        window.alert('Número 1 pressionado.')
-    } else if(num2Value == 1){
-        window.alert('Número 2 pressionado.')
+        num1Value = 0;
+        num1.style.color = '';
+        num1.style.backgroundColor = '';
     }
+
+     //Pressionar3
+     if (num3Value == 1){
+        num3Value = 0;
+        num3.style.color = '';
+        num3.style.backgroundColor = '';
+    }
+
+    //pressionar4
+    if (num4Value == 1){
+        num4Value = 0;
+        num4.style.color = '';
+        num4.style.backgroundColor = '';
+    }
+
+    //pressionar2
+    if (num2Value == 1){
+        num2Value = 0;
+        num2.style.color = '';
+        num2.style.backgroundColor = '';
+    }
+}
+
+
+function pressionar(){
+   
 }
